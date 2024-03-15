b2f62b790b7934cb2b1c43355c38dc6eff12b4a6
howhowtocook <xiaoln2000@gmail.com>
2024-03-16 01:38:54 +0800

    add button and event listener

--- rockpaperscissors.js
+++ rockpaperscissors.js
@@ -1,43 +1,56 @@
 const lists = ['rock','paper','scissors']
     // Your JavaScript code goes here
-    function getComputerChoice(){
-        var randomNumber = Math.floor(Math.random() * 3) ;
-        return lists[randomNumber];
-    };
+function getComputerChoice(){
+    var randomNumber = Math.floor(Math.random() * 3) ;
+    return lists[randomNumber];
+};
 
-    function playRound(playerSelection, computerSelection) {
-        var key = playerSelection.toLowerCase() + "-" + computerSelection;
-        let result;
-        switch (key) {
-            case "rock-scissors":
-            case "scissors-paper":
-            case "paper-rock":
-                result = 1;
-                break;
-            case "rock-rock":
-            case "scissors-scissors": 
-            case "paper-paper":
-                result = 0;
-                break;
-            case "scissors-rock":  
-            case "paper-scissors":
-            case "rock-paper":
-                result = -1;
-                break;
-            }
-        return result;   
+function playRound(playerSelection, computerSelection) {
+    var key = playerSelection.toLowerCase() + "-" + computerSelection;
+    let result;
+    switch (key) {
+        case "rock-scissors":
+        case "scissors-paper":
+        case "paper-rock":
+            result = 1;
+            break;
+        case "rock-rock":
+        case "scissors-scissors": 
+        case "paper-paper":
+            result = 0;
+            break;
+        case "scissors-rock":  
+        case "paper-scissors":
+        case "rock-paper":
+            result = -1;
+            break;
+        }
+    return result;   
 }
 
-function PlayGame(){
-    total_score = 0;
-    for (i=0;i<5;i++){
-        const playerSelection = prompt("Enter your selection:");
-        const computerSelection = getComputerChoice();
-        let score = playRound(playerSelection, computerSelection);
-        total_score += score;
+const body = document.querySelector("body");
+// buttons is a node list. It looks and acts much like an array.
+const buttons = document.querySelectorAll("button");
+total_score = 0;
+// we use the .forEach method to iterate through each button
+buttons.forEach((button) => {
+  // and for each one we add a 'click' listener
+  button.addEventListener("click", () => {
+    const result = document.createElement('div');
+    score = playRound(button.id,getComputerChoice());
+    result.textContent = score;
+    body.appendChild(result)
+    total_score += score;
+    console.log(total_score);
+
+    const total_result = document.createElement('div');
+    if (total_score >= 5){
+    total_result.textContent = "you win";
     }
-    return total_score;
-}
-
-
-console.log(PlayGame())
+    if (total_score <= -5){
+    total_result.textContent = "you lose";
+    
+    }
+    body.appendChild(total_result)
+  });
+});
